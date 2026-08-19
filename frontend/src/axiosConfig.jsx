4efb976bc8f4f2ad646dcabe6ab4143087e6c8a2
Kou <koutarokuwahara@gmail.com>
2026-08-19 23:06:39 +0000

    Revert "Use REACT_APP_API_URL env var for API base URL"
    This reverts commit 46ee07f641a11e3fe5495e6eaf2f871fb115d70d.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
