8e28df55a57f20b733a708df7705dfff6a21505d
Kou_Kuwahara <koutarokuwahara@gmail.com>
2026-08-24 11:45:00 +1000

    add user roles to auth and user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     university: { type: String },
     address: { type: String },
+    role: { type: String, enum: ['user', 'agent'], default: 'user' },
 });
 
 userSchema.pre('save', async function (next) {
