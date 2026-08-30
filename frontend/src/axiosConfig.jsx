16639ac68bbbafed3f8381380582de26fa8b3381
Kou_Kuwahara <koutarokuwahara@gmail.com>
2026-08-30 11:47:08 +1000

    added EC2 instance IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.107.172.160:5001', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://13.211.174.176:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
